a0f3ad473f857ebe52b0462dc0b3c52e89b4eb77
wuyinlei <wuyinlei19931118@hotmail.com>
2016-03-05 20:07:34 +0800

    加入四种路线规划和定位功能，这里的文件有点乱，如果需要定位功能的时候，请在mainfest文件中把定位的activity作为启动

--- src/main/java/com/example/mylocation/MainActivity.java
+++ src/main/java/com/example/mylocation/MainActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.search.busline.BusLineResult;
+import com.baidu.mapapi.search.busline.BusLineSearch;
+import com.baidu.mapapi.search.busline.BusLineSearchOption;
+import com.baidu.mapapi.search.busline.OnGetBusLineSearchResultListener;
 import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
 import com.baidu.mapapi.search.poi.PoiCitySearchOption;
 import com.baidu.mapapi.search.poi.PoiDetailResult;
@@ -17,13 +24,22 @@ import com.baidu.mapapi.search.poi.PoiSearch;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnGetPoiSearchResultListener, OnGetBusLineSearchResultListener {
 
     private MapView mapView;
-    private Button poiClick;
+    private Button poiClick, nextClick;
+
+    private int nodeIndex = -2;// 节点索引,供浏览节点时使用
+    private List<String> busLineIDList = null;
+    private int busLineIndex = 0;
 
     //第一步，创建POI对象
     private PoiSearch mPoiSearch;
+
+
+    private BusLineSearch mBusLineSearch;
+
+    private String busLineId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,20 +54,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPoiSearch = PoiSearch.newInstance();
 
 
-        OnGetPoiSearchResultListener poiListener = new OnGetPoiSearchResultListener(){
-            public void onGetPoiResult(PoiResult result){
-                //获取POI检索结果
-                List<PoiInfo> allAddr = result.getAllPoi();
-                for (PoiInfo p: allAddr) {
-                    Log.d("MainActivity", "p.name--->" + p.name +"p.phoneNum" + p.phoneNum +" -->p.address:" + p.address + "p.location" + p.location);
-                }
-            }
-            public void onGetPoiDetailResult(PoiDetailResult result){
-                //获取Place详情页检索结果
-            }
-        };
+        mBusLineSearch = BusLineSearch.newInstance();
 
-        mPoiSearch.setOnGetPoiSearchResultListener(poiListener);
+        mBusLineSearch.setOnGetBusLineSearchResultListener(this);
+
+        mPoiSearch.setOnGetPoiSearchResultListener(this);
 
     }
 
@@ -61,12 +68,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mapView.onDestroy();
         mPoiSearch.destroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -76,9 +85,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        mPoiSearch.searchInCity((new PoiCitySearchOption())
+       /* mPoiSearch.searchInCity((new PoiCitySearchOption())
                 .city("北京")
                 .keyword("ktv")
-                .pageNum(10));
+                .pageNum(10));*/
+        switch (v.getId()) {
+            case R.id.poiClick:
+                poiClick();
+
+                break;
+            case R.id.nextClick:
+                break;
+        }
+
+    }
+
+
+    private void poiClick() {
+        mPoiSearch.searchInCity((new PoiCitySearchOption())
+                .city("北京")
+                .keyword("717"));
+
+    }
+
+    @Override
+    public void onGetBusLineResult(BusLineResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(this, "抱歉，未找到结果",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(this, result.getBusLineName(),
+                Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", result.toString());
+        List<BusLineResult.BusStation> steps = result.getStations();
+        StringBuffer sb = new StringBuffer();
+        for (BusLineResult.BusStation b:steps) {
+            sb.append("-->");
+            sb.append(b.getTitle());
+        }
+        Log.d("MainActivity", sb.toString());
+    }
+
+    @Override
+    public void onGetPoiResult(PoiResult result) {
+
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(this, "抱歉，未找到结果",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        // 遍历所有poi，找到类型为公交线路的poi
+        for (PoiInfo poi : result.getAllPoi()) {
+            if (poi.type == PoiInfo.POITYPE.BUS_LINE
+                    || poi.type == PoiInfo.POITYPE.SUBWAY_LINE) {
+                //如下代码为发起检索代码，定义监听者和设置监听器的方法与POI中的类似
+                mBusLineSearch.searchBusLine((new BusLineSearchOption()
+                        //我这里的城市写死了，和我要查的是一样的
+                        .city("北京")
+                        .uid(poi.uid)));
+            }
+        }
+    }
+
+    @Override
+    public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
     }
 }
